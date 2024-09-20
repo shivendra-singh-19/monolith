1a339ac7ff2e8bc9061cc4dcaf5f2edbd6705dec
Shivendra Singh <shivendrasingh@Shivendras-MacBook-Pro.local>
2024-09-20 18:43:19 +0530

    Add: :Added scheduler bullMq implementation bench mark

--- src/api/scheduler/SchedulerApi.ts
+++ src/api/scheduler/SchedulerApi.ts
@@ -1,7 +1,9 @@
-import Queue from 'bull';
+import BullQueue from 'bull';
+import { Job, Queue } from 'bullmq';
 import { addMinutes, addSeconds, getTime } from 'date-fns';
 import dotenv from 'dotenv';
 import { RedisOptions } from 'ioredis';
+import mongoose from 'mongoose';
 dotenv.config();
 
 const redisOptions: RedisOptions = {
@@ -9,7 +11,12 @@ const redisOptions: RedisOptions = {
   port: Number(process.env.REDIS_PORT),
 };
 
-const cronJobQueue = new Queue('RecurringTask', { redis: redisOptions });
+const cronJobQueue = new BullQueue('RecurringTask', { redis: redisOptions });
+const cronBullMqQueue = new Queue('Bull-Recurring-Tasks', {
+  connection: redisOptions,
+});
+
+// cronBullMqQueue.setGlobalConcurrency(10);
 
 export class SchedulerApi {
   /**
@@ -54,6 +61,93 @@ export class SchedulerApi {
 
     return {
       message: 'Added 1 test job to queue',
+    };
+  }
+
+  /**
+   * Adding job in bull mq
+   * @param object
+   * @param options
+   */
+  static async addBullMqJob(object, options) {
+    const startTime = new Date();
+    const thirtySecondsLater = addSeconds(new Date(), 30);
+    for (let i = 0; i < 10000; i++) {
+      const uniqueId = new mongoose.Types.ObjectId();
+      await cronBullMqQueue.add(
+        uniqueId.toString(),
+        { name: 'Himanshu', uniqueId: uniqueId, startTime },
+        {
+          delay: getTime(thirtySecondsLater) - getTime(new Date()),
+          // repeat: {
+          //   pattern: '* * * * * *',
+          //   limit: 20,
+          // },
+          removeOnComplete: true,
+          removeOnFail: true,
+        }
+      );
+    }
+
+    return {
+      message: 'Job has been added to the bull mq',
+    };
+  }
+
+  /**
+   * Change delay time
+   * @param object
+   * @param options
+   */
+  static async changeDelayInJob(object, options) {
+    const { delay: newDelay } = object;
+
+    const job = await Job.create(
+      cronBullMqQueue,
+      'RecurringTask',
+      { name: 'Shivendra Singh' },
+      { delay: newDelay }
+    );
+  }
+
+  /**
+   * Adding recurring bull mq job
+   * @param object
+   * @param options
+   * @returns
+   */
+  static async addRecurringBullMqJob(object, options) {
+    await cronBullMqQueue.add(
+      'RecurringTask',
+      { name: 'Shivendra Singh' },
+      {
+        repeat: {
+          pattern: '*/5 * * * * *',
+          limit: 10,
+        },
+        removeOnComplete: {
+          //Keep job for 1 hour
+          age: 3600,
+          //Keep 1000 jobs
+          count: 1000,
+        },
+        removeOnFail: true,
+      }
+    );
+
+    return {
+      message: 'recurring Job has been added to the bull mq',
+    };
+  }
+
+  static async removeRecurringJob(object, options) {
+    await cronBullMqQueue.removeRepeatable('RecurringTask', {
+      pattern: '*/5 * * * * *',
+      limit: 10,
+    });
+
+    return {
+      message: 'Job removed',
     };
   }
 }
--- src/api/scheduler/routes.ts
+++ src/api/scheduler/routes.ts
@@ -7,3 +7,15 @@ export const ScheduledJobsRouter = express.Router();
 ScheduledJobsRouter.post('/', api.http(SchedulerApi.addJob));
 
 ScheduledJobsRouter.post('/bench', api.http(SchedulerApi.add10kJobs));
+
+ScheduledJobsRouter.post('/v2', api.http(SchedulerApi.addBullMqJob));
+
+ScheduledJobsRouter.post(
+  '/v2/recurring',
+  api.http(SchedulerApi.addRecurringBullMqJob)
+);
+
+ScheduledJobsRouter.delete(
+  '/v2/recurring',
+  api.http(SchedulerApi.removeRecurringJob)
+);
